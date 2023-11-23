8e357179346139707c9ebaed122daaa222cea056
kulaphongj <kulaphong.j@gmail.com>
2023-11-22 22:15:55 -0800

    move function get Q name from QuizParser to QuizManager

--- quizzes/quiz_parser.py
+++ quizzes/quiz_parser.py
@@ -11,26 +11,3 @@ class QuizParser:
 
 	def __init__(self, quizname):
 		self.quizname = quizname  # name of .json file
-
-
-	def get_all_pari_qname_file(self):
-
-		# Use os.path.join to create the full path
-		pattern = os.path.join(path_dbquizzes, '*.json')
-		# Use glob to get all files matching the pattern
-		json_files = glob.glob(pattern)
-
-		dict_pair_qname_file = {}
-		for json_file in json_files:
-			f = open(json_file)
-			data = json.load(f)
-			qname = data['qname']
-			dict_pair_qname_file[qname] = json_file
-			f.close()
-
-		return dict_pair_qname_file
-
-
-	def get_all_quiz_name(self):
-		list_quiz_name = list(self.get_all_pari_qname_file().keys())
-		return list_quiz_name
