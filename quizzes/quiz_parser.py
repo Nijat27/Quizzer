13a56d9601237d43e02ff5152029d3a04dbb10b7
kulaphongj <kulaphong.j@gmail.com>
2023-11-23 11:27:51 -0800

    update get all quiz name function

--- quizzes/quiz_parser.py
+++ quizzes/quiz_parser.py
@@ -9,5 +9,23 @@ path_dbquizzes = os.path.join(path_cur, 'db_quizzes')
 
 class QuizParser:
 
-	def __init__(self, quizname):
-		self.quizname = quizname  # name of .json file
+	def __init__(self):
+		# self.quizname = quizname  # name of .json file
+		pass 
+
+	def get_all_pair_qname_file(self):
+
+		# Use os.path.join to create the full path
+		pattern = os.path.join(path_dbquizzes, '*.json')
+		# Use glob to get all files matching the pattern
+		json_files = glob.glob(pattern)
+
+		dict_pair_qname_file = {}
+		for json_file in json_files:
+			f = open(json_file)
+			data = json.load(f)
+			qname = data['quizname']
+			dict_pair_qname_file[qname] = json_file
+			f.close()
+
+		return dict_pair_qname_file
--- ui_qmanager/quiz_manager.py
+++ ui_qmanager/quiz_manager.py
@@ -2,6 +2,8 @@ import os
 import glob
 import json
 
+from quizzes.quiz_parser import QuizParser
+
 path_cur = os.getcwd()
 # print(path_cur)
 path_dbquizzes = os.path.join(path_cur, 'db_quizzes')
@@ -12,24 +14,7 @@ class QuizManager:
 	def __init__(self):
 		pass
 
-	def get_all_pair_qname_file(self):
-
-		# Use os.path.join to create the full path
-		pattern = os.path.join(path_dbquizzes, '*.json')
-		# Use glob to get all files matching the pattern
-		json_files = glob.glob(pattern)
-
-		dict_pair_qname_file = {}
-		for json_file in json_files:
-			f = open(json_file)
-			data = json.load(f)
-			qname = data['qname']
-			dict_pair_qname_file[qname] = json_file
-			f.close()
-
-		return dict_pair_qname_file
-
-
 	def get_all_quiz_name(self):
-		list_quiz_name = list(self.get_all_pair_qname_file().keys())
+		qp = QuizParser()
+		list_quiz_name = list(qp.get_all_pair_qname_file().keys())
 		return list_quiz_name
