b11c47e6bf5f30a19a103b17632e8569556913fd
Nijat27 <nabliz@student.ubc.ca>
2023-12-02 15:54:04 -0800

    main_menu Doc string

--- quizuiflowcontrol/main_menu.py
+++ quizuiflowcontrol/main_menu.py
@@ -7,22 +7,27 @@ from quizuiflowcontrol.quizmanager import QuizManager
 
 class QuizApp:
     def __init__(self):
+        """Initialize the QuizApp with an empty user name and a QuizManager instance."""
         self.user_name = ""
         self.quiz_manager = QuizManager(path_db_quizzes)  # Replace with the actual path
 
     def run(self):
+         """Start the main loop of the application."""
         self.display_welcome_message()
         self.ask_user_name()
         self.main_menu()
 
     def display_welcome_message(self):
+        """Display a welcome message to the user."""
         print("Welcome to the Quiz App")
 
     def ask_user_name(self):
+        """Ask the user to input their name and store it."""
         self.user_name = input("What is your name? ")
         print(f"Welcome, {self.user_name}!\n")
 
     def main_menu(self):
+        """ Display the menu and handle user selections."""
         while True:
             self.show_menu_options()
             user_choice = input("Your selection? ")
@@ -44,21 +49,26 @@ class QuizApp:
                 self.display_error_message()
 
     def show_menu_options(self):
+        """Show menu options."""
         print("Please make a selection")
         print("(L): List quizzes")
         print("(T): Take a quiz")
         print("(E): Exit the App")
 
     def display_error_message(self):
+        """Display an error message for invalid selections."""
         print("Not a valid selection. Try again!")
 
     def exit_app(self):
+        """Exit the application."""
         print("Exiting the Quiz App. Goodbye!")
 
     def list_quizzes(self):
+        """List all available quizzes."""
         self.quiz_manager.list_quizzes()
 
     def take_quiz(self):
+        """Hande the logic of taking a quiz."""
         try:
             quiz_number = int(input("Enter the quiz number: "))
             if quiz_number in self.quiz_manager.quizzes:
