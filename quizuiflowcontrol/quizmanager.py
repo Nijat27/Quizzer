c4d66d0a0d438f0352fc5700ad502a853b205b21
Nijat27 <nabliz@student.ubc.ca>
2023-12-02 16:19:04 -0800

    updated the quiz manager file

--- quizuiflowcontrol/quizmanager.py
+++ quizuiflowcontrol/quizmanager.py
@@ -4,50 +4,88 @@ import glob
 from quizutils.quizparser import JSONQuizParser
 
 class QuizManager:
+    """
+    Manages the quizzes, including listing, taking, and saving results of quizzes.
+
+    Attributes:
+        quizfolder (str): Path to the directory containing quiz files.
+        the_quiz (Quiz): Currently selected quiz object.
+        quizzes (dict): Dictionary mapping quiz IDs to quiz objects.
+        results (Result): Results of the last taken quiz.
+        quiztaker (str): Name of the user taking the quiz.
+    """
+
     def __init__(self, quizfolder):
-        """Initialize the QuizManager with the specified quiz folder."""
+        """
+        Initialize the QuizManager with a specific quiz folder path.
+
+        Args:
+            quizfolder (str): The path to the folder containing quiz files.
+
+        Raises:
+            FileNotFoundError: If the specified quiz folder does not exist.
+        """
         self.quizfolder = quizfolder
         self.the_quiz = None
         self.quizzes = {}
         self.results = None
         self.quiztaker = ""
 
+        # Check if the quiz folder exists
         if not os.path.exists(quizfolder):
             raise FileNotFoundError("The quiz folder doesn't exist!")
 
+        # Build the list of quizzes
         self._build_quiz_list()
 
     def _build_quiz_list(self):
-        """Method to build a list of quizzes from JSON files in the quiz folder."""
+        """
+        Private method to build a list of quizzes from JSON files in the quiz folder.
+        """
+        # Define the pattern to search for JSON files
         pattern = os.path.join(self.quizfolder, '*.json')
         json_files = glob.glob(pattern)
 
+        # Parse each JSON file and add to quizzes
         for i, f in enumerate(json_files):
             parser = JSONQuizParser()
             self.quizzes[i + 1] = parser.parse_quiz(f)
-            # if f.is_file():
-            #     print(f)
-            #     parser = JSONQuizParser()
-            #     self.quizzes[i + 1] = parser.parse_quiz(f.path)
 
     def list_quizzes(self):
-        """Display a list of all available quizzes."""
+        """
+        Display a list of all available quizzes to the user.
+        """
+        # Print each quiz in the list
         for k, v in self.quizzes.items():
             print(f"({k}): {v.name}")
 
     def take_quiz(self, quizid, username):
-        """Take a quiz with the given ID and record results for the specified username."""
+        """
+        Facilitate taking a quiz by a user.
+
+        Args:
+            quizid (int): The ID of the quiz to take.
+            username (str): The name of the user taking the quiz.
+
+        Returns:
+            Result: The results of the quiz.
+        """
         self.quiztaker = username
         self.the_quiz = self.quizzes[quizid]
         self.results = self.the_quiz.take_quiz()
         return self.results
 
     def print_results(self):
-        """Print the results of the quiz taken by the user."""
+        """
+        Print the results of the last taken quiz.
+        """
         self.the_quiz.print_results(self.quiztaker)
 
     def save_results(self):
-        """Save the results of the quiz in a file."""
+        """
+        Save the results of the quiz in a uniquely named file.
+        """
+        # Generate a unique filename for the results
         today = datetime.datetime.now()
         filename = f"QuizResults_{today.year}_{today.month}_{today.day}.txt"
         n = 1
@@ -55,6 +93,7 @@ class QuizManager:
             filename = f"QuizResults_{today.year}_{today.month}_{today.day}_{n}.txt"
             n += 1
 
+        # Save the results in the specified file
         path_cur = os.getcwd()
         path_result = os.path.join(path_cur, 'db_results', filename)
         with open(path_result, "w") as f:
