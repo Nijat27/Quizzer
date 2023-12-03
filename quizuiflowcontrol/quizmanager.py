fd43badba7b5cb79e3c58e5ac2b9ae04ab4d6242
Nijat27 <nabliz@student.ubc.ca>
2023-12-02 16:02:44 -0800

    updated the QuizManager with Docstrings

--- quizuiflowcontrol/quizmanager.py
+++ quizuiflowcontrol/quizmanager.py
@@ -5,6 +5,7 @@ from quizutils.quizparser import JSONQuizParser
 
 class QuizManager:
     def __init__(self, quizfolder):
+        """Initialize the QuizManager with the specified quiz folder."""
         self.quizfolder = quizfolder
         self.the_quiz = None
         self.quizzes = {}
@@ -17,6 +18,7 @@ class QuizManager:
         self._build_quiz_list()
 
     def _build_quiz_list(self):
+        """Method to build a list of quizzes from JSON files in the quiz folder."""
         pattern = os.path.join(self.quizfolder, '*.json')
         json_files = glob.glob(pattern)
 
@@ -29,19 +31,23 @@ class QuizManager:
             #     self.quizzes[i + 1] = parser.parse_quiz(f.path)
 
     def list_quizzes(self):
+        """Display a list of all available quizzes."""
         for k, v in self.quizzes.items():
             print(f"({k}): {v.name}")
 
     def take_quiz(self, quizid, username):
+        """Take a quiz with the given ID and record results for the specified username."""
         self.quiztaker = username
         self.the_quiz = self.quizzes[quizid]
         self.results = self.the_quiz.take_quiz()
         return self.results
 
     def print_results(self):
+        """Print the results of the quiz taken by the user."""
         self.the_quiz.print_results(self.quiztaker)
 
     def save_results(self):
+        """Save the results of the quiz in a file."""
         today = datetime.datetime.now()
         filename = f"QuizResults_{today.year}_{today.month}_{today.day}.txt"
         n = 1
