62cb780cb25c1c6a2fb820f0ddd2c7f6a7a05ad6
Nijat27 <nabliz@student.ubc.ca>
2023-12-11 11:59:45 -0800

    unittests

--- unittest_main.py
+++ unittest_main.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest_quizzer.unittest_quiz import TestQuiz
 from unittest_quizzer.unittest_quizparser import TestQuizParser
+from unittest_quizzer.unit_test_main import TestQuizApp
+from unittest_quizzer.unit_test_quizmanager import TestQuizManager
 
 
 def test_suite():
@@ -8,19 +10,25 @@ def test_suite():
     """
     suite = unittest.TestSuite()
     result = unittest.TestResult()
+    suite.addTest(TestQuizApp('test_initialization'))
+    suite.addTest(TestQuizApp('test_ask_user_name'))
+    suite.addTest(TestQuizApp('test_main_menu_list_quizzes'))
+    suite.addTest(TestQuizApp('test_main_menu_take_quiz'))
+    suite.addTest(TestQuizApp('test_main_menu'))
+    suite.addTest(TestQuizManager('test_initialization'))
+    suite.addTest(TestQuizManager('test_build_quiz_list'))
+    suite.addTest(TestQuizManager('test_list_quizzes'))
+    suite.addTest(TestQuizManager('test_take_quiz'))
     suite.addTest(TestQuizParser('test_import_data'))
     suite.addTest(TestQuizParser('test_create_question_obj'))
     suite.addTest(TestQuizParser('test_parse_quiz'))
-
     suite.addTest(TestQuiz('test_print_header'))
     suite.addTest(TestQuiz('test_print_results'))
     # suite.addTest(TestQuiz('test_take_quiz'))  # take_quiz is required user input.
     suite.addTest(TestQuiz('test_cal_score'))
-    
+
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 
 
 test_suite()
-
-
